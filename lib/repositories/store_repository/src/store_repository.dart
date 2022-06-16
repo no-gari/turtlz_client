0d5fb29bca8a9bfc320bbf7f59a3240b9b64b3a6
no-gari <sofaissofa@icloud.com>
2022-06-16 17:26:04 +0900

    added flutter sizer

--- lib/repositories/store_repository/src/store_repository.dart
+++ lib/repositories/store_repository/src/store_repository.dart
@@ -1,9 +1,8 @@
 import 'package:turtlz/repositories/store_repository/models/collection.dart';
-import 'package:turtlz/repositories/store_repository/models/menu.dart';
-import 'package:turtlz/support/networks/api_result.dart';
-import 'package:turtlz/support/networks/dio_client.dart';
 import 'package:turtlz/support/networks/network_exceptions.dart';
 import 'package:turtlz/support/networks/page_response.dart';
+import 'package:turtlz/support/networks/api_result.dart';
+import 'package:turtlz/support/networks/dio_client.dart';
 
 class CollectionGetFailure implements Exception {}
 
