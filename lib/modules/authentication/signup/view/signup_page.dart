c0db0daeadf598b2ce4a84e6adc78c13c2082419
no-gari <sofaissofa@icloud.com>
2022-07-01 11:41:02 +0900

    fixed signup page

--- lib/modules/authentication/signup/view/signup_page.dart
+++ lib/modules/authentication/signup/view/signup_page.dart
@@ -1,5 +1,4 @@
 import 'package:turtlz/repositories/authentication_repository/authentication_repository.dart';
-import 'package:turtlz/modules/authentication/signup/view/signup_screen.dart';
 import 'package:turtlz/modules/authentication/signin/cubit/signin_cubit.dart';
 import 'package:turtlz/modules/authentication/bloc/authentication_bloc.dart';
 import 'package:turtlz/support/style/format_unit.dart';
@@ -40,107 +39,115 @@ class _SignupPageState extends State<SignupPage> {
           automaticallyImplyLeading: true,
         ),
         body: BlocListener<AuthenticationBloc, AuthenticationState>(
-          listener: (context, state) async {
-            if (state.status == AuthenticationStatus.authenticated) {
-              context.vRouter.to(MainScreen.routeName, isReplacement: true);
-            }
-          },
-          child: BlocListener<SignInCubit, SignInState>(
-              listener: (context, state) async {
-                if (state.errorMessage != null &&
-                    state.errorMessage!.length > 0) {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                    content: Text("이메일 혹은 비밀번호를 확인해 주세요."),
-                  ));
-                  context.read<SignInCubit>().errorMsg();
-                }
-              },
-              child: SingleChildScrollView(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('sign up.',
-                            style: Theme.of(context).textTheme.headline3),
-                        const SizedBox(height: 30),
-                        const Text("이메일"),
-                        TextFormField(
-                            controller: emailController,
-                            autofocus: true,
-                            decoration: InputDecoration(
-                                enabledBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                focusedBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                border: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                hintText: '이메일을 입력 해주세요.')),
-                        const SizedBox(height: 20),
-                        const Text("비밀번호"),
-                        TextFormField(
-                            controller: passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                enabledBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                focusedBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                border: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                hintText: '비밀번호를 입력 해주세요.')),
-                        const SizedBox(height: 20),
-                        const Text("비밀번호 확인"),
-                        TextFormField(
-                            controller: passwordConfirmController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                enabledBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                focusedBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                border: UnderlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: Theme.of(context).primaryColor)),
-                                hintText: '비밀번호를 확인 해주세요.')),
-                        const SizedBox(height: 30),
-                        GestureDetector(
-                            onTap: () {
-                              if (passwordController.text !=
-                                  passwordConfirmController.text) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                        content: Text("비밀번호가 서로 일치하지 않습니다.")));
-                              } else {
-                                if (emailController.text.trim() != '' &&
-                                    passwordController.text.trim() != '') {
-                                  _signInCubit.signUpWithEmail(
-                                      email: emailController.text,
-                                      password: passwordController.text);
+            listener: (context, state) async {
+              if (state.status == AuthenticationStatus.authenticated) {
+                context.vRouter.to(MainScreen.routeName, isReplacement: true);
+              }
+            },
+            child: BlocListener<SignInCubit, SignInState>(
+                listener: (context, state) async {
+                  if (state.errorMessage != null &&
+                      state.errorMessage!.length > 0) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("이메일 혹은 비밀번호를 확인해 주세요."),
+                    ));
+                    context.read<SignInCubit>().errorMsg();
+                  }
+                },
+                child: SingleChildScrollView(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('sign up.',
+                              style: Theme.of(context).textTheme.headline3),
+                          const SizedBox(height: 30),
+                          const Text("이메일"),
+                          TextFormField(
+                              controller: emailController,
+                              autofocus: true,
+                              decoration: InputDecoration(
+                                  enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  border: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  hintText: '이메일을 입력 해주세요.')),
+                          const SizedBox(height: 20),
+                          const Text("비밀번호"),
+                          TextFormField(
+                              controller: passwordController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  border: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  hintText: '비밀번호를 입력 해주세요.')),
+                          const SizedBox(height: 20),
+                          const Text("비밀번호 확인"),
+                          TextFormField(
+                              controller: passwordConfirmController,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  border: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                  hintText: '비밀번호를 확인 해주세요.')),
+                          const SizedBox(height: 30),
+                          GestureDetector(
+                              onTap: () {
+                                if (passwordController.text !=
+                                    passwordConfirmController.text) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content:
+                                              Text("비밀번호가 서로 일치하지 않습니다.")));
+                                } else {
+                                  if (emailController.text.trim() != '' &&
+                                      passwordController.text.trim() != '') {
+                                    _signInCubit.signUpWithEmail(
+                                        email: emailController.text,
+                                        password: passwordController.text);
+                                  }
                                 }
-                              }
-                            },
-                            child: Container(
-                                width: maxWidth(context),
-                                height: 60,
-                                decoration: BoxDecoration(
-                                    color: Theme.of(context).primaryColor,
-                                    borderRadius: BorderRadius.circular(20)),
-                                child: Center(
-                                    child: Text("회원가입",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline5!
-                                            .copyWith(color: Colors.white))))),
-                        const SizedBox(height: 30),
-                      ]))),
-        ));
+                              },
+                              child: Container(
+                                  width: maxWidth(context),
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                      color: Theme.of(context).primaryColor,
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: Center(
+                                      child: Text("회원가입",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline5!
+                                              .copyWith(color: Colors.white)))))
+                        ])))));
   }
 }
